c0a46e716bf1d1cef6e158f65e53b6bfb21cbc09
kazvee <kayvee123@gmail.com>
2023-08-31 17:24:42 -0600

    Add data files

---
+++ src/data/ducks.js
@@ -0,0 +1,29 @@
+const ducks = [
+  {
+    name: "Daisy",
+    emoji: "🦆",
+    favoriteFood: "Apple pie",
+  },
+  {
+    name: "Webby",
+    emoji: "🦆",
+    favoriteFood: "Popcorn",
+  },
+  {
+    name: "Gosalyn",
+    emoji: "🦆",
+    favoriteFood: "Pancakes",
+  },
+  {
+    name: "Howard",
+    emoji: "🦆",
+    favoriteFood: "Cinnamon rolls",
+  },
+  {
+    name: "Quackers",
+    emoji: "🦆",
+    favoriteFood: "Poutine",
+  },
+];
+
+export default ducks;
---
+++ src/data/monkeys.js
@@ -0,0 +1,94 @@
+const monkeys = [
+  {
+    name: "Banana",
+    emoji: "🙈",
+    favoriteFood: "Coconuts",
+    species: "Pygmy Marmoset",
+    victoryMessages: [
+      "Peel the victory!",
+      "Cracked it!",
+      "Top of the tree!"
+    ],
+    defeatMessages: [
+      "Monkey business as usual.",
+      "Slipped on a banana peel of luck.",
+      "A bunch of misses.",
+      "Back to the drawing board.",
+      "Caught in a fruitless cycle."
+    ]
+  },
+  {
+    name: "Coco",
+    emoji: "🙉",
+    favoriteFood: "Peanuts",
+    species: "Capuchin Monkey",
+    victoryMessages: [
+      "Nut-tastic triumph!",
+      "The nutty winner!",
+      "Going coconuts!"
+    ],
+    defeatMessages: [
+      "Not peeling very pleased.",
+      "Nuts about losing.",
+      "This is bananas!",
+      "Cracking under pressure.",
+      "Shell-shocked disappointment."
+    ]
+  },
+  {
+    name: "Peanut",
+    emoji: "🙊",
+    favoriteFood: "Bananas",
+    species: "Tamarin Monkey",
+    victoryMessages: [
+      "Going nutty for victory!",
+      "Bananas for the win!",
+      "Peanut power prevails!"
+    ],
+    defeatMessages: [
+      "I'm peeling a bit heartbroken.",
+      "Cracking down on defeat.",
+      "Aw, nuts!",
+      "Lost in a nutty maze.",
+      "Feeling a bit shell-shocked."
+    ]
+  },
+  {
+    name: "Mango",
+    emoji: "🐵",
+    favoriteFood: "Macadamia nuts",
+    species: "Squirrel Monkey",
+    victoryMessages: [
+      "Mango magic prevails!",
+      "Macadamia mastery!",
+      "Nuts for victory!"
+    ],
+    defeatMessages: [
+      "Mango mishap strikes again.",
+      "Peeling the weight of defeat.",
+      "Nutty dreams, nutty reality.",
+      "Cracking under the pressure.",
+      "Macadamia moment missed."
+    ]
+  },
+  {
+    name: "Luna",
+    emoji: "🐒",
+    favoriteFood: "Mangoes",
+    species: "Japanese Macaque (Snow Monkey)",
+    victoryMessages: [
+      "Lunar victory shines!",
+      "My success eclipses you!",
+      "Moonbeam mastery!"
+    ],
+    defeatMessages: [
+      "Astronomically bad luck.",
+      "Lost in a lunar orbit.",
+      "Feeling eclipsed by defeat.",
+      "Moonstruck by loss.",
+      "Unlucky lunar landing."
+    ]
+  },
+];
+
+export default monkeys;
