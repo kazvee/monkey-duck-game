27b65904eb8506d442164e2b4216bee810b92ae5
kazvee <kayvee123@gmail.com>
2023-09-02 11:24:48 -0600

    Implement addDucks functionality with button

--- src/components/ShowEmojis.jsx
+++ src/components/ShowEmojis.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import shuffleArray from '../helpers/shuffleArray';
+import ducks from '../data/ducks';
+import addDucks from '../helpers/addDucks';
 
 const ShowEmojis = ({ emojis }) => {
+  const [displayedEmojis, setDisplayedEmojis] = useState([]);
 
-  const shuffledEmojis = shuffleArray([...emojis]);
+  useEffect(() => {
+    const shuffledEmojis = shuffleArray([...emojis]);
+    setDisplayedEmojis(shuffledEmojis);
+  }, [emojis]);
+
+  const handleAddDucks = () => {
+    const finalEmojis = addDucks(displayedEmojis, ducks);
+    const emojiStrings = finalEmojis.map((item) => {
+      if (typeof item === 'object' && item.emoji) {
+        return item.emoji;
+      }
+      return item;
+    });
+    setDisplayedEmojis(emojiStrings);
+  };
 
   return (
     <div>
       <h1>Emojis</h1>
+      <button onClick={handleAddDucks}>Add Ducks</button>
       <div className="emoji-container">
-        {shuffledEmojis.map((emoji, index) => (
+        {displayedEmojis.map((emoji, index) => (
           <span key={index} className="emoji">
             {emoji}
           </span>
