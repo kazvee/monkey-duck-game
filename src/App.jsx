f59c57cdff14a1d2bf99ffe17ed8d882eb29fc3d
kazvee <kayvee123@gmail.com>
2023-09-02 12:20:43 -0600

    Implement ShowMonkey functionality with button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ShowEmojis from "../src/components/ShowEmojis";
 import emojis from "../src/data/emojis";
+import ShowMonkey from "../src/components/ShowMonkey";
 
 const App = () => {
   return (
     <div className="App">
       <ShowEmojis emojis={emojis} />
+      <ShowMonkey />
     </div>
   );
 };
