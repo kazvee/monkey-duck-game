8f397c65229eb3a9cd3dc6257f92bc4b2f232e32
kazvee <kayvee123@gmail.com>
2023-09-02 19:17:24 -0600

    Remove unneeded ShowMonkey component from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ShowEmojis from "../src/components/ShowEmojis";
 import emojis from "../src/data/emojis";
-import ShowMonkey from "../src/components/ShowMonkey";
 
 const App = () => {
   return (
     <div className="App">
       <ShowEmojis emojis={emojis} />
-      <ShowMonkey />
     </div>
   );
 };
