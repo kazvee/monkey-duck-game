bf471ac857cc6bd88a75b2f2ee1a133f874604d8
kazvee <kayvee123@gmail.com>
2023-09-04 19:25:23 -0600

    Add Header and Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      Image by <a href="https://www.freepik.com/free-vector/flat-design-fruit-floral-pattern-illustration_28492546.htm#&position=0&from_view=collections" target="_blank" rel="noopener noreferrer">Freepik</a>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Welcome to the Monkey Duck game! 🐒🦆</h1>
+    </header>
+  );
+};
+
+export default Header;
