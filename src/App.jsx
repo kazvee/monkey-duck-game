75fbe71d3b2e05a2f52af592369c628032568f2b
kazvee <kayvee123@gmail.com>
2023-09-05 17:48:38 -0600

    Display Header and Footer on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ShowEmojis from "../src/components/ShowEmojis";
 import emojis from "../src/data/emojis";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <ShowEmojis emojis={emojis} />
+      <Footer />
     </div>
   );
 };
