2736a55dfdd40cf90bf6dfa54d93f02d74535697
kazvee <kayvee123@gmail.com>
2023-10-06 13:18:23 -0600

    Update Footer content & styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,26 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      Background Image by <a href="https://www.freepik.com/free-vector/flat-design-fruit-floral-pattern-illustration_28492546.htm#&position=0&from_view=collections" target="_blank" rel="noopener noreferrer">Freepik</a>
+      <span>
+        Made with{' '}
+        <a
+          href='https://github.com/kazvee/monkey-duck-game'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          💚{' '}
+        </a>
+        and a beautiful background image from{' '}
+        <a
+          href='https://www.freepik.com/free-vector/flat-design-fruit-floral-pattern-illustration_28492546.htm#&position=0&from_view=collections'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Freepik
+        </a>
+      </span>
     </footer>
   );
-}
+};
 
 export default Footer;
